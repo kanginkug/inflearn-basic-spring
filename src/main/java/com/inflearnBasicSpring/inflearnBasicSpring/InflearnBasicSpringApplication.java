23df414b897d73586dbec6c84900e19c199cf2f6
kik <rkddlsrnr1234@gmail.com>
2023-08-22 12:13:36 +0900

    first commit

---
+++ src/main/java/com/inflearnBasicSpring/inflearnBasicSpring/InflearnBasicSpringApplication.java
@@ -0,0 +1,13 @@
+package com.inflearnBasicSpring.inflearnBasicSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnBasicSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnBasicSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inflearnBasicSpring/inflearnBasicSpring/InflearnBasicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inflearnBasicSpring.inflearnBasicSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnBasicSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
