a1609359da72102cf9fd6d3c04b6cc72082e72e5
kik <rkddlsrnr1234@gmail.com>
2023-08-22 12:33:24 +0900

    create member, MemberRepository, MemoryMemberRepository

---
+++ src/main/java/com/inflearnBasicSpring/inflearnBasicSpring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.inflearnBasicSpring.inflearnBasicSpring.repository;
+
+import com.inflearnBasicSpring.inflearnBasicSpring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
